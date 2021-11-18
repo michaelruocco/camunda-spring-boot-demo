642520171b24b3f76684900bc72cef328d96d141
Michael Ruocco <michael.ruocco@hotmail.com>
2021-11-18 08:11:27 +0000

    fix javascript warnings flagged by codacy

--- src/main/resources/META-INF/resources/webjars/camunda/app/cockpit/custom/logout.js
+++ src/main/resources/META-INF/resources/webjars/camunda/app/cockpit/custom/logout.js
@@ -1,15 +1,13 @@
 let observer = new MutationObserver(() => {
-  // find the logout button
   const logoutButton = document.querySelectorAll(".logout > a")[0];
-  // once the button is present replace it with new functionality
   if (logoutButton) {
-    var parent = logoutButton.parentElement
-    parent.removeChild(logoutButton)
-    var newLogout = document.createElement('a');
-    newLogout.setAttribute('className', 'ng-binding')
-    newLogout.innerText = logoutButton.innerText.replaceAll('\n', '');
-    newLogout.setAttribute('href', 'logout'); // call server side logout handler
-    parent.appendChild(newLogout)
+    var parent = logoutButton.parentElement;
+    parent.removeChild(logoutButton);
+    var newLogout = document.createElement("a");
+    newLogout.setAttribute("className", "ng-binding");
+    newLogout.innerText = logoutButton.innerText.replaceAll("\n", "");
+    newLogout.setAttribute("href", "logout");
+    parent.appendChild(newLogout);
     observer.disconnect();
   }
 });
--- src/main/resources/META-INF/resources/webjars/camunda/app/tasklist/scripts/config.js
+++ src/main/resources/META-INF/resources/webjars/camunda/app/tasklist/scripts/config.js
@@ -1,5 +1,5 @@
 export default {
   customScripts: [
-    'custom/logout'
+    "custom/logout"
   ]
 };
